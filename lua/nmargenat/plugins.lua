ea8c31903451b2f0f9f1dd601fe304cb92ff6c64
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-07-23 20:46:32 -0300

    Agregado autocompletado

--- lua/nmargenat/plugins.lua
+++ lua/nmargenat/plugins.lua
@@ -44,10 +44,21 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  use 'tanvirtin/monokai.nvim' -- Colorscheme
 
+  -- Colorscheme
+  use 'tanvirtin/monokai.nvim' 
 
-  -- Automatically set up your configuration after cloning packer.nvim
+    -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+ -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use  -- Automatically set up your configuration after cloning packer.nvim
+
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
     require("packer").sync()
